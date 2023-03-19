c9cd66ebb7245ad5e8bb3fb5f6cafaeedd6b35fb
Lee Roland <glroland@hotmail.com>
2023-03-19 10:18:29 -0400

    functioning config server framework

--- mayhem-control/src/main/java/com/glroland/mayhem/control/MayhemControlServerApplication.java
+++ mayhem-control/src/main/java/com/glroland/mayhem/control/MayhemControlServerApplication.java
@@ -2,12 +2,14 @@ package com.glroland.mayhem.control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class MayhemControlServerApplication {
-
-	public static void main(String[] args) {
+@EnableConfigServer
+public class MayhemControlServerApplication 
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(MayhemControlServerApplication.class, args);
 	}
-
 }
